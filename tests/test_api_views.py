b9332e62de52f31df37c762a2a8db5ed2569234e
DimitryRukhadze <lokiinflames@gmail.com>
2023-11-18 00:34:01 +0300

    added first parametrized test

--- tests/test_api_views.py
+++ tests/test_api_views.py
@@ -10,21 +10,13 @@ from api.views import get_form
 def request_factory():
     return APIRequestFactory()
 
-
-@pytest.mark.django_db
-def test_get_form(request_factory):
-    data = {
+@pytest.mark.parametrize('request_body,expected_response', [
+    ({
         'message': 'blahblah',
         'post_date': '2008-02-12',
         'user_author': 'mememe',
         'author_email': 'author_email'
-    }
-
-    request = request_factory.post('/get-form', data)
-    response = get_form(request)
-    assert response.status_code == 200
-    assert isinstance(response, JsonResponse)
-    assert json.loads(response.content) == [
+    }, [
         {
         "template_name": "new_message",
         "user_author": "text",
@@ -32,4 +24,14 @@ def test_get_form(request_factory):
         "message": "text",
         "post_date": "date"
         }
-    ]
+    ]),
+
+])
+@pytest.mark.django_db
+def test_get_form(request_factory, request_body, expected_response):
+
+    request = request_factory.post('/get-form', request_body)
+    response = get_form(request)
+    assert response.status_code == 200
+    assert isinstance(response, JsonResponse)
+    assert json.loads(response.content) == expected_response
